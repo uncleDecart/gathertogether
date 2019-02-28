42747ab43ab8102974f6bd3bd22f095cedd20e94
Komaricus <33253356+Komaricus@users.noreply.github.com>
2019-02-28 22:53:11 +0300

    More TURN servers

--- server.js
+++ server.js
@@ -47,9 +47,29 @@ var myIceServers = [{
         "url": "stun:stun.sipgate.net:10000"
     },
     {
-        "url": "134.209.84.230:3487",
-        "username": "test",
-        "credential": "test"
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+    },
+    {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    },
+    {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    },
+    {
+        url: 'turn:turn.bistri.com:80',
+        credential: 'homeo',
+        username: 'homeo'
+    },
+    {
+        url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+        credential: 'webrtc',
+        username: 'webrtc'
     }
 ];
 
